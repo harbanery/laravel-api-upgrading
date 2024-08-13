c31fb13575f6d226fbb20541a497e66875829bfe
harbanery <ryusuf05@gmail.com>
2024-08-13 12:24:11 +0700

    updated model users & model transactions

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,6 +68,6 @@ class User extends Authenticatable
 
     public function transactions()
     {
-        // return $this->hasMany(Transaction::class, 'users.id', 'id');
+        return $this->hasMany(Transaction::class, 'users_id', 'id');
     }
 }
